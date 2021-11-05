b46da7326eac6a92c40a24bfe2c9af8332943624
adnan-sabbir <adnan.sabbir@g.bracu.ac.bd>
2021-11-05 09:30:37 +0600

    UI enhancement

--- src/components/number-selector-modal/number-selector-modal.js
+++ src/components/number-selector-modal/number-selector-modal.js
@@ -4,16 +4,27 @@ import './number-selector-modal.style.css';
 
 Modal.setAppElement('#root');
 
+const INITIAL_VALUE = {value: '', invalid: true};
+
 export const NumberSelectorModal = ({openModal, setModalState, setStartingNumber}) => {
-    const [inputValue, setInputValue] = useState(0);
+    const [inputValue, setInputValue] = useState(INITIAL_VALUE);
 
     const handleValueChange = (e) => {
-        setInputValue(parseInt(e.target.value, 10));
+        const value = e.target.value;
+        const data = {value, invalid: (!value || +value < 20 || +value > 100)}
+        setInputValue(data);
     }
 
     const handleSubmit = () => {
-        setStartingNumber(inputValue)
+        if (inputValue.invalid) return;
+        setStartingNumber(+inputValue);
+        closeModal();
     }
+
+    const closeModal = () => {
+        setInputValue(INITIAL_VALUE);
+        setModalState(false);
+    };
 
     return (
         <Modal
@@ -24,15 +35,31 @@ export const NumberSelectorModal = ({openModal, setModalState, setStartingNumber
 
             <div className="header">
                 <h1 className="title">How many people?</h1>
-                <span className="cross-icon" onClick={() => setModalState(false)}>X</span>
+                <span className="cross-icon" onClick={closeModal}>X</span>
             </div>
             <div className="body">
-                <p className="description text-dark-gray">Enter a number of how many people you want to add to the list.</p>
-                <input className="input-box" type="number" value={inputValue} onChange={handleValueChange}/>
+                <p className="description text-dark-gray">
+                    Enter a number of how many people you want to add to the list.
+                </p>
+                <input
+                    placeholder="Enter a number between 20 to 100"
+                    className="input-box"
+                    type="number"
+                    value={inputValue.value} onChange={handleValueChange}
+                    min={20} max={100}/>
             </div>
             <div className="button-group">
-                <button className="button bg-light-gray text-dark-gray" onClick={() => setModalState(false)}>Cancel</button>
-                <button className="button bg-orange text-white" onClick={handleSubmit}>Start</button>
+                <button
+                    className="button bg-light-gray text-dark-gray"
+                    onClick={closeModal}>
+                    Cancel
+                </button>
+                <button
+                    className="button bg-orange text-white"
+                    onClick={handleSubmit}
+                    disabled={inputValue.invalid}>
+                    Start
+                </button>
             </div>
         </Modal>
     )
